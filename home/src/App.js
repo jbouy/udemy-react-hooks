10020fc54260323277cda2c68d45c2ed2cb4b1b1
James Bouy <jbouy@turner-industries.com>
2019-06-18 21:47:44 -0500

    s2l6: First Hook: UseState

--- home/src/App.js
+++ home/src/App.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return <div className="App">App</div>;
-}
+const App = () => {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value);
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello James</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button type="submit" onClick={searchQuery}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
